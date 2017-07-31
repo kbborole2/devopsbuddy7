642ece96f2e265c0fd226df6bc57ac4f5893b9e8
Khetho Borole <kbborole2@gmail.com>
2017-07-31 14:30:26 -0700

    Initial project commit

---
+++ src/main/java/com/devopsbuddy7/Devopsbuddy7Application.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devopsbuddy7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devopsbuddy7Application.class, args);
+	}
+}
